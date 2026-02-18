0752b1d6dca57bc18be7760d4e0fda9882366d28
tinamengxq <tinam5@uci.edu>
2026-02-17 19:17:56 -0800

    1

--- GDIM32 Final/Assets/Scripts/NPC.cs
+++ GDIM32 Final/Assets/Scripts/NPC.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
-    [SerializeField]float interactionMaximumDistance = 5f;
+    [SerializeField]private float interactionMaximumDistance = 5f;
+    public GameObject _dialogueUI;
+    public GameObject _interactionUI;
+
+    public Transform player;
+    private bool playerIsClose = false;
 
 
     void Start()
     {
-        
+        _dialogueUI.SetActive(false);
+        _interactionUI.SetActive(false);
     }
-
 
     void Update()
     {
